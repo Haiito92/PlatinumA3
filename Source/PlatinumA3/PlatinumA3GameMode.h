7aa4ba37f9882e9f1d4e81e7223adbefee862f9e
Antoine Hanna <hannaantoinebusiness@gmail.com>
2024-10-09 10:39:28 +0200

    [INIT] Commit

---
+++ Source/PlatinumA3/PlatinumA3GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlatinumA3GameMode.generated.h"
+
+UCLASS(minimalapi)
+class APlatinumA3GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APlatinumA3GameMode();
+};
+
+
+

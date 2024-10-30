de403555bfa58a50a294fe33108e539d008727c5
Ibrahim-Mallouka <panda.bornalex@gmail.com>
2024-10-30 14:01:55 +0100

    [Add] Base Spline Camera

---
+++ Source/PlatinumA3/Private/Runtime/Camera/SplineCameraScroller.cpp
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Runtime/Camera/SplineCameraScroller.h"
+
+#include "Camera/CameraActor.h"
+#include "Components/SplineComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Runtime/Characters/WoolStateCharacter.h"
+
+
+// Sets default values
+ASplineCameraScroller::ASplineCameraScroller()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	m_SplineComponent = CreateDefaultSubobject<USplineComponent>(TEXT("SplineComponent"));
+}
+
+// Called when the game starts or when spawned
+void ASplineCameraScroller::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	
+}
+
+// Called every frame
+void ASplineCameraScroller::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	UpdateSplineCamera();
+}
+
+void ASplineCameraScroller::InitializedSplineCameraScroller(AWoolStateCharacter* InBerger, AWoolStateCharacter* InChien)
+{
+	if(!InBerger || !InChien) return;
+	
+	m_Berger = InBerger;
+	m_Chien = InChien;
+	
+
+	m_CameraActor = Cast<ACameraActor>(UGameplayStatics::GetActorOfClass(GetWorld(), TSubclassOf<ACameraActor>()));
+
+	if(m_CameraActor)
+	{
+		
+	}else
+	{
+		
+	}
+}
+
+void ASplineCameraScroller::UpdateSplineCamera()
+{
+	if(!m_Berger || !m_Chien || !m_CameraActor) return;
+	
+	FVector InterpolatedLocation = FMath::Lerp(m_Berger->GetActorLocation(), m_Chien->GetActorLocation(), 0.5f);
+
+	FVector CameraTargetLocation = m_SplineComponent->FindLocationClosestToWorldLocation(InterpolatedLocation, ESplineCoordinateSpace::World);
+
+	m_CameraActor->SetActorLocation(CameraTargetLocation);
+}
+
---
+++ Source/PlatinumA3/Public/Runtime/Camera/SplineCameraScroller.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SplineCameraScroller.generated.h"
+
+class USplineComponent;
+class AWoolStateCharacter;
+
+UCLASS()
+class PLATINUMA3_API ASplineCameraScroller : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ASplineCameraScroller();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SplineCameraScroller")
+	TObjectPtr<USplineComponent> m_SplineComponent;
+
+	UPROPERTY(BlueprintReadWrite, Category = "SplineCameraScroller")
+	TObjectPtr<ACameraActor> m_CameraActor;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "SplineCameraScroller")
+	TObjectPtr<AWoolStateCharacter> m_Berger;
+
+	UPROPERTY(BlueprintReadWrite, Category = "SplineCameraScroller")
+	TObjectPtr<AWoolStateCharacter> m_Chien;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	UFUNCTION()
+	void InitializedSplineCameraScroller(AWoolStateCharacter* InBerger, AWoolStateCharacter* InChien);
+
+	UFUNCTION()
+	void UpdateSplineCamera();
+};

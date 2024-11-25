b1ae874a4688e7c54349475be836ddc5dd60e3cd
Antoine Hanna <hannaantoinebusiness@gmail.com>
2024-11-25 15:33:02 +0100

    [ADD] Cheats System folder

---
+++ Source/PlatinumA3/Public/Editor/CheatsSystem/UI/CheatsMenu/CheatsMenu.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CheatsMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLATINUMA3_API UCheatsMenu : public UUserWidget
+{
+	GENERATED_BODY()
+};
